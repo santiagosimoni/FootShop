33dc206ad6c615dc953a9d1c61ab2139b37356e5
santiagosimoni <santiagosimoni24@gmail.com>
2023-03-16 13:20:15 -0300

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
             <h2>Footshop</h2>
             <h5>Web creada con reactJS </h5>
             <section className='redes-footer'>
-              <a href="#"><img src="\src\imagenes\instagram-original (2).svg" alt="icono instagram" /></a>
-              <a href="#"><img src=".\src\imagenes\facebook-fill.svg" alt="icono facebook" /></a>
-              <a href="#"><img src=".\src\imagenes\twitter (1).svg" alt="icono twitter" /></a>
+              <a href="#"><img src="..\src\imagenes\instagram-original (2).svg" alt="icono instagram" /></a>
+              <a href="#"><img src="..\src\imagenes\facebook-fill.svg" alt="icono facebook" /></a>
+              <a href="#"><img src="..\src\imagenes\twitter (1).svg" alt="icono twitter" /></a>
             </section>
           </div>
       </div>
